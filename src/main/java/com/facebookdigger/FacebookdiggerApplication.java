d511337784004ebd957bab4bf2acac3b3d7d0b71
ejaksla <jakub.slawinski@ericsson.com>
2017-02-18 23:35:47 +0100

    Initial commit - beta version - under development

---
+++ src/main/java/com/facebookdigger/FacebookdiggerApplication.java
@@ -0,0 +1,12 @@
+package com.facebookdigger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookdiggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacebookdiggerApplication.class, args);
+    }
+}
